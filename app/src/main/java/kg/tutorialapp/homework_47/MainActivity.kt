c66e68fb86580f8da5fe97557ad0297c6db76590
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-07-03 22:16:50 +0600

    registered the receiver programmatically and added edittext for entering messages

--- app/src/main/java/kg/tutorialapp/homework_47/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/homework_47/MainActivity.kt
@@ -1,15 +1,20 @@
 package kg.tutorialapp.homework_47
 
 
+import android.content.BroadcastReceiver
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val myReceiver: BroadcastReceiver = Receiver()
+
     private val myAction = "kg.tutorialapp.action.MESSAGE"
-    private val alarmMessage = "Hello world!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +25,34 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        val filter = IntentFilter("kg.tutorialapp.action.MESSAGE")
+        registerReceiver(myReceiver, filter)
+
+        Toast.makeText(applicationContext, "Приёмник включен", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        unregisterReceiver(myReceiver)
+
+        Toast.makeText(applicationContext, "Приёмник выключён", Toast.LENGTH_SHORT).show()
+    }
+
     private fun sendMessage() {
+        val editMessage = findViewById<EditText>(R.id.editMessage)
+        val message = editMessage.text.toString()
+
         val intent = Intent()
         intent.action = myAction
-        intent.putExtra("kg.tutorialapp.broadcast.MESSAGE", alarmMessage)
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+        intent.putExtra("kg.tutorialapp.broadcast.MESSAGE", message)
         sendBroadcast(intent)
     }
+
+
 
 }
--- app/src/main/java/kg/tutorialapp/homework_47/Receiver.kt
+++ app/src/main/java/kg/tutorialapp/homework_47/Receiver.kt
@@ -5,10 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
+
 class Receiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
         Toast.makeText(context, "Новое сообщение: " + intent.getStringExtra("kg.tutorialapp.broadcast.MESSAGE"), Toast.LENGTH_LONG).show()
     }
+
 }
